a70722958f265073bc77137fb0e9f17c7c01432f
tronghuuphan <pthuu91@gmail.com>
2021-09-08 19:27:29 +0700

    Update person log feature

--- DatabaseTool.py
+++ DatabaseTool.py
@@ -3,6 +3,9 @@ from aws_config import read_db_config, read_aws_config
 from upload_image import upload_to_aws
 
 def get_all_id():
+    '''
+    Get all ID from from the database (Person Table)
+    '''
     query = """
     SELECT id FROM LogApp_person
     """
@@ -23,6 +26,9 @@ def get_all_id():
         
 
 def get_camera_id():
+    '''
+    Get all camera ID from database (Camera Table)
+    '''
     query = """
     SELECT id FROM LogApp_camera
     """
@@ -42,6 +48,11 @@ def get_camera_id():
         conn.close()
 
 def insert_person(name, image_file):
+    '''
+    Insert a person (name) into database (Person Table)
+    param name: Name of person. If unknow, name = ""
+    param image_file: NAME of image file (NOT PATH. If PATH, please preprocess)
+    '''
     query = """
     INSERT INTO LogApp_person(name)
     VALUES (%s)"""
@@ -65,6 +76,7 @@ def insert_tracking(idPeople_id, idContact_id, idCam_id, date, time):
     """
     NOTE: Date format: "yyyy-mm-dd"
           Time format: "hh:mm:ss"
+    Example: insert_tracking(idPeople_id=1, idContact_id=2, idCam_id=4, date='2020-01-01', time='10:23:23')
     """
 
     query = """
@@ -84,3 +96,36 @@ def insert_tracking(idPeople_id, idContact_id, idCam_id, date, time):
     finally:
         cursor.close()
         conn.close()
+
+
+# New feature: Upload personal Log
+def insert_person_log(idPeople_id, idCam_id, mask, date, time, image):
+    """
+    Upload into person log
+    Note:
+        param mask: Boolean type (0 or 1)
+
+    Example: insert_person_log(idPeople_id=2, idCam_id=1, mask=1,'2020-01-01','12:23:22','image_of_id.jpg')
+    """
+
+    query = """
+    INSERT INTO LogApp_personlog(idPeople_id, idCam_id, mask, date, time, image)
+    VALUES (%s,%s,%s,%s,%s,%s)
+    """
+    args = (idCam_id, idCam_id, mask, date, time, 'person-log.s3.ap-southeast-1.amazonaws.com/' + image)
+    try:
+        db_config = read_db_config()
+        conn = MySQLConnection(**db_config)
+
+        cursor = conn.cursor()
+        cursor.execute(query, args)
+
+        conn.commit()
+        upload_to_aws(image, image, bucket='person-log')
+
+    except Error as e:
+        print(e)
+    finally:
+        cursor.close()
+        conn.close()
+
